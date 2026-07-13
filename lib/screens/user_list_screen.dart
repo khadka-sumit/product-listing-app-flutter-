c9da04cc1308a6a90970ac3c797a8b5ea7407ef3
khadka-sumit <sumitkhadka708@gmail.com>
2026-07-13 12:40:00 +0545

    Add: ListView.builder to display users

---
+++ lib/screens/user_list_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import '../models/user.dart';
+import '../services/api_service.dart';
+
+class UserListScreen extends StatefulWidget {
+  const UserListScreen({super.key});
+
+  @override
+  State<UserListScreen> createState() => _UserListScreenState();
+}
+
+class _UserListScreenState extends State<UserListScreen> {
+  late Future<List<User>> _usersFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _usersFuture = ApiService.fetchUsers();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('JSONPlaceholder Users')),
+      body: FutureBuilder<List<User>>(
+        future: _usersFuture,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final users = snapshot.data!;
+            return ListView.builder(
+              itemCount: users.length,
+              itemBuilder: (context, index) {
+                final u = users[index];
+                return ListTile(
+                  leading: CircleAvatar(child: Text('${u.id}')),
+                  title: Text(u.name),
+                  subtitle: Text(u.email),
+                );
+              },
+            );
+          }
+          return const Center(child: CircularProgressIndicator());
+        },
+      ),
+    );
+  }
+}
